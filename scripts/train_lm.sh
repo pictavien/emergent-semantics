b09172b4a8ddb6511b8ff4eefb84dfa634f5038e
Gerald Point <gerald.point@labri.fr>
2024-10-14 13:15:03 +0200

    decrease eval batch size

--- scripts/train_lm.sh
+++ scripts/train_lm.sh
@@ -14,7 +14,7 @@ python -m lm.train \
     --block_size 2048 \
     --num_train_epochs 3 \
     --per_device_train_batch_size 2 \
-    --per_device_eval_batch_size 16 \
+    --per_device_eval_batch_size 8 \
     --overwrite_cache \
     --gradient_accumulation_steps 8 \
     --checkpointing_steps 2000 \
